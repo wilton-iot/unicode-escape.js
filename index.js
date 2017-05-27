56804598cfc101eb73e86b88cc630c9f48317aea
alex <alex@staticlibs.net>
2017-05-27 14:07:35 +0100

    wilton convert

--- index.js
+++ index.js
@@ -1,5 +1,8 @@
+define(function(){var require = WILTON_requiresync;var module = {exports: {}};var exports = module.exports;
 module.exports = function(str) {
   return str.replace(/./g, function(c) {
     return "\\u" + ('000' + c.charCodeAt(0).toString(16)).substr(-4);
   });
 };
+
+return module.exports;});
