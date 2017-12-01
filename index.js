bae0fe1fd52f2f7ee27acd79ce2a2d6380be6f4b
alex <alex@staticlibs.net>
2017-12-01 00:21:25 +0000

    browser compat

--- index.js
+++ index.js
@@ -1,8 +1,8 @@
-define(function(){var require = WILTON_requiresync;var module = {exports: {}};var exports = module.exports;
+define(function(localRequire, exports, module) { var requireOrig = require; require = localRequire;
 module.exports = function(str) {
   return str.replace(/./g, function(c) {
     return "\\u" + ('000' + c.charCodeAt(0).toString(16)).substr(-4);
   });
 };
 
-return module.exports;});
+require = requireOrig;});
